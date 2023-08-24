830de57c7b092e8629d45c491661097f5fb139da
Kiryl Pryhozhy <kirill19962003@gmail.com>
2023-08-24 10:19:22 +0300

    init: init project

---
+++ src/main/java/ru/clevertec/sm/RestfulStateMachineApplication.java
@@ -0,0 +1,13 @@
+package ru.clevertec.sm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulStateMachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulStateMachineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/clevertec/sm/RestfulStateMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.clevertec.sm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulStateMachineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
